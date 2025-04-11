39f08bddfde49e785a43b43a9ed147ba81079ddf
Rohan <rohansingh8382@gmail.com>
2025-04-12 04:59:49 +0530

    login-signUp page

--- src/Components/loginSignup/loginSignUp.jsx
+++ src/Components/loginSignup/loginSignUp.jsx
@@ -1,38 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './loginSignUp.css'
 
 import user_icon from '../Assets/user.png'
 import email_icon from '../Assets/email.svg'
 import password_icon from '../Assets/password.png'
 
-const loginSignUp = () => {
+const LoginSignUp = () => {
+
+const [action,setAction] = useState("Login");
+
+
   return (
     <div className='container'>
       <div className="header">
-        <div className="text">SignUp</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
-        <div className="input">
+        {action==="Login"?<div></div>:<div className="input">
             <img class ="icon" src={user_icon} alt="" />
-            <input type="text" />
-        </div>
+            <input type="text" placeholder='Name' />
+        </div>}
+        
         <div className="input">
             <img class ="icon" src={email_icon} alt="" />
-            <input type="email" />
+            <input type="email" placeholder='Email'/>
         </div>
         <div className="input">
             <img class ="icon" src={password_icon} alt="" />
-            <input type="password" />
+            <input type="password" placeholder='Password' />
         </div>
       </div>
-      <div className="forgot-password">Forgot Password? <span>Click Here!!</span></div>
+      {action==="Sign Up"? <div></div>:<div className="forgot-password">Forgot Password? <span>Click Here!!</span></div>}
+      
       <div className="submit-container">
-        <div className="submit">Sign Up</div>
-        <div className="submit">Login</div>
+        <div className={action ==="Login"?"submit gray":"submit"} onClick={()=> {setAction("Sign Up")}}>Sign Up</div>
+        <div className={action ==="Sign Up"?"submit gray":"submit"} onClick={()=> {setAction("Login")}}>Login</div>
       </div>
     </div>
   )
 }
 
-export default loginSignUp
+export default LoginSignUp
